a7b63ec92ab10a44d0428df0bdd56377096889a4
kevgarher <kevingarciah03@gmail.com>
2026-08-17 01:26:50 +0200

    add health endpoint

--- app/routes.py
+++ app/routes.py
@@ -4,10 +4,13 @@ from app import models
 
 router = APIRouter(prefix="/api/v1", tags=["players"])
 
-
 @router.get("/players", response_model=models.JugadorResponse)
 def get_player(player_name: str = Query(...)):
     player_stats = estadisticas.get_player_stats(player_name)
     if not player_stats:
         raise HTTPException(status_code=404, detail="Player not found")
     return player_stats
+
+@router.get("/players/health")
+def health():
+    return {"status": "ok"}
