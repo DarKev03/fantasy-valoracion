2f4286327d53239aa32849155e0b57cc08c1f8cc
kevgarher <kevingarciah03@gmail.com>
2026-08-17 12:06:48 +0200

    calcualar oferta con diferencia con precio real figurado

--- app/estadisticas_fantasy/estadisticas.py
+++ app/estadisticas_fantasy/estadisticas.py
@@ -23,7 +23,7 @@ def get_player_stats(name: str, season: int = 2026) -> models.JugadorResponse | 
         name=player_name,
         market_value=market_value,
         media_rating=media_rating,
-        oportunity_score=oportunity_score
+        calculated_score=oportunity_score
     )
     
     
@@ -32,11 +32,11 @@ def get_player_name(data: dict, name: str) -> tuple[str, int] | None:
     i = 0
     for player in data.get("data", []):
         i += 1
-        if (name.lower()) in player["playerName"].lower():
-            return player["playerName"], i-1
+        if (name.lower()) in player["nickname"].lower():
+            return player["nickname"], i-1
     return None
 
-def get_score(player: dict) -> str:
+def get_score(player: dict) -> models.CalculatedScore:
     POSITIONS = {
     1: 0.561,
     2: 0.369,
@@ -45,16 +45,40 @@ def get_score(player: dict) -> str:
 }
     position = player.get("positionId")
     market_value = player.get("marketValue")
-    media_rating = player.get("averagePoints")
-    score = calculate_score(market_value, media_rating)
-
-    if score > POSITIONS.get(position, 0):
-        return "Buena oportunidad"
-    else:
-        return "Mala oportunidad"
+    media_rating = player.get("averagePoints")    
+    posible_real_price = calculate_real_price(media_rating, POSITIONS.get(position, 0))   
+    difference = posible_real_price - market_value 
+    difference_percentage = (difference / market_value) * 100
+    score_text = get_score_text(difference_percentage)
+    
+    return models.CalculatedScore(
+        score=score_text,
+        posible_real_price=round(posible_real_price, 2),
+        difference=round(difference, 2),
+        difference_percentage=f'{difference_percentage:.2f}%'
+    )           
 
 def calculate_score(market_value: float, media_rating: float) -> float:
     return ((media_rating / market_value) * 1000000)
+
+def calculate_real_price(media_rating: float, position_value: float) -> float:    
+    return (media_rating / position_value) * 1000000
+
+def get_score_text(difference_percentage: float) -> str:
+    if difference_percentage >= 50:
+        return "Super ganga"
+    elif difference_percentage >= 25 and difference_percentage < 50:
+        return "Ganga"
+    elif difference_percentage >= 10 and difference_percentage < 25:
+        return "Buena oportunidad"
+    elif difference_percentage >= -10 and difference_percentage < 10:
+        return "Precio justo"
+    elif difference_percentage >= -25 and difference_percentage < -10:
+        return "Caro"
+    elif difference_percentage >= -50 and difference_percentage < -25:
+        return "Muy caro"
+    elif difference_percentage < -50:
+        return "Extremadamente Sobrevalorado"    
 
 if __name__ == "__main__":
     data = get_season_summary()
--- app/models.py
+++ app/models.py
@@ -1,11 +1,16 @@
 from pydantic import BaseModel
 
+class JugadorRequest(BaseModel):
+    name: str
+
+class CalculatedScore(BaseModel):
+    score: str
+    posible_real_price: float
+    difference: float
+    difference_percentage: str
 
 class JugadorResponse(BaseModel):
     name: str
     market_value: float
     media_rating: float
-    oportunity_score: str
-
-class JugadorRequest(BaseModel):
-    name: str
+    calculated_score: CalculatedScore
